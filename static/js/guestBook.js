ef9ceed83d53761408029629979c7f8a9945c49a
JM <k3371548@gmail.com>
2022-11-03 17:25:43 +0900

    guestBook 기능 고침

--- static/js/guestBook.js
+++ static/js/guestBook.js
@@ -8,8 +8,15 @@ let guestBook = {
 
         this.postBox = $('#postBox');
 
+        this.registEvent();
     }
     ,registEvent : function(){
+
+        let _this = this;
+        $('#fil').on('keyup', function(){
+            _this.listFilter();
+        });
+
     }
 
     ,settingList : function(){
@@ -29,7 +36,7 @@ let guestBook = {
                         let temp_html = `<div class="card" id="listCard${oid}">
                                         <div class="card-body">
                                             <blockquote class="blockquote mb-0">
-                                                <p>${conents}</p>
+                                                <p class="contents">${conents}</p>
                                                 <footer class="blockquote-footer">${name}</footer>
                                             </blockquote>
                                              <button onclick="guestBook.openUpdateForm('${oid}')" type="button" class="btn btn-outline-dark">수정</button>
@@ -40,6 +47,41 @@ let guestBook = {
                     }
                 }
         });
+    }
+
+    ,listFilter : function(){
+        let value, contents, nick, item, i;
+
+            value = document.getElementById("fil").value;
+            // "fil"아이디에 포함된 모든 문자열의 입력값을 value에 넣어준다. 쉽게 보면 글검색창에 단어를 입력시 value에 넣어준다
+            // 라고 생각하면됨
+
+            item = document.getElementsByClassName("card");
+            // "block"클래스, 즉 이미 있는 글내용과 닉네임이 있는 클래스에 포함된 모든 문자열을 item에 넣어준다.
+
+            for (i = 0; i < item.length; i++) {
+                // item에는 item[0], item[1], item[2] 현재 이렇게 담겨져있다.
+
+                contents = item[i].getElementsByClassName("contents");
+                // 각 아이템 배열에서 클래스 contents, 즉 글내용 부분을 담는다
+                console.log(contents)
+                nick = item[i].getElementsByClassName("blockquote-footer");
+                // 각 아이템 배열에서 클래스 nick, 즉 닉네임 부분을 담는다
+                if (contents[0].innerHTML.indexOf(value) > -1 ||
+                    nick[0].innerHTML.indexOf(value) > -1) {
+                    // innerHTML은 HTML의 컨텐츠, 즉 내용에 접근할 수 있는 변수이고
+                    // indexOf()는 괄호안 값 문자열의 위치가 0,1,2,3,4... 인지 반환, 만약 없으면 -1을 반환한다.
+
+                    //만약 첫번째 글내용에 HTML의 컨텐츠값인 value(이건 검색창에서 입력한 문자열)가 존재하거나
+                    //|| 혹은 첫번째 닉네임에 HTML의 컨텐츠값인 value(이건 검색창에서 입력한 문자열)이 존재할경우
+
+                    item[i].style.display = "block";
+                    // 지금 전체 글내용중에서 해당 번째의 글내용을 보여준다
+                } else {
+                    item[i].style.display = "none";
+                    // 지금 전체 글내용중에서 해당 번째의 글내용을 보여주지 않는다.
+                }
+            }
     }
 
     ,insertGuestBook : function(){
@@ -159,6 +201,7 @@ let guestBook = {
     ,removeEvent : function(){
 
     }
+
     ,destroy: function(){
 
     }
